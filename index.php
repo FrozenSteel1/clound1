70460989a94da6f7ec255f922c4197e73c0864de
frozensteel1 <FrozenSteel@mail.ru>
2021-04-19 16:41:23 +0300

    afs

---
+++ index.php
@@ -0,0 +1,25 @@
+<!doctype html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="css.css">
+    <title>Приложение для верификации видео данных</title>
+</head>
+<body>
+<header>
+    <a  <h1 class="nameSystemLink">Верификатор</h1> </a>
+        <a <span>Регистрация</span></a>
+        <a <span>Вход</span></a>
+
+</header>
+<section>
+    <a <span>Настройки</span></a>
+    <link rel="stylesheet" href="css/css.css">
+</section>
+<footer>
+</footer>
+</body>
+</html>
